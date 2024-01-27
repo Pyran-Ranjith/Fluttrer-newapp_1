81bec9a3f5867e0ecbb95b1613ca96be10354386
PyraN-Ranjith Jayawardana <pyranranjith@gmail.com>
2024-01-27 07:12:51 +0530

    min.drat file added

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var textButton =
+        TextButton(onPressed: () {}, child: const Text('Forgot password?'));
+    return MaterialApp(
+        // For more features we use Scaffold with body
+        home: Scaffold(
+            backgroundColor: Colors.blue[400],
+            body: Column(children: [
+              const Text('Hello welcome back'),
+              const Text('Login continue'),
+              const TextField(
+                decoration: InputDecoration(hintText: 'User name'),
+              ),
+              const TextField(
+                decoration: InputDecoration(hintText: 'Password'),
+              ),
+              textButton,
+              ElevatedButton(onPressed: () {
+                print('Login is clicked');
+              }, child: const Text('Login')),
+                Text('Or sign in with'),
+                ElevatedButton(onPressed: () {
+                print('Goegle is clicked');
+                }, child: 
+                const Text('Login with Google'))
+            ])));
+  }
+}
